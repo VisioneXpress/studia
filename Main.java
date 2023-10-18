f65c0718633f55ca13df65c7fe37d780fa8608dd
VisioneXpress <118394744+VisioneXpress@users.noreply.github.com>
2023-10-18 18:57:10 +0200

    zadania zjazd 2
    zadania 1-5 ze zjazdu 2 
    1.	Napisz funkcję, która będzie przyjmowała jako parametr liczbę całkowitą i będzie zwracała silnię.
    2.	Napisz funkcję, która przyjmuje jako parametr pozycję w ciągu Fibonacciego i zwraca wynik (liczbę) tej pozycji. 
    3.	Napisz funkcję, która przyjmuje dwie liczby całkowite jako parametry i wypisuje liczby nieparzyste. Wypisywanie powinno się zaczynać od pierwszego parametru i kończyć na drugim także wtedy gdy drugi parametr jest mniejszy od pierwszego.
    4.	Napisz dwie tak samo nazywające się funkcje, ale jednak przyjmujące jako parametr liczbę całkowitą a druga bez parametru. Ta z parametrem powinna wypisywać dzień tygodnie zależnie od liczby podanej w parametrze (od 1 do 7) lub wypisać napis „nieprawidłowa liczba”. Ta bez parametru powinna wypisywać aktualny dzień tygodnia. Skorzystaj z klasy LocalDate.
    5.	Napisz funkcję, która przyjmuje dowolną liczbę parametrów - liczb całkowitych bądź zmiennoprzecinkowych i wypisuje ich medianę, średnią oraz sumę.

--- Main.java
+++ Main.java
@@ -1,40 +1,23 @@
 public class Main {
+    public static int obliczSilnie(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Silnia jest zdefiniowana tylko dla liczb nieujemnych.");
+        }
 
-    public static void main(String[] args) {
-        obliczStatystyki(2, 4, 6, 8, 10);
-        obliczStatystyki(3.5, 1.5, 4.0, 2.0, 5.5);
-        obliczStatystyki(1, 2, 3);
+        if (n == 0) {
+            return 1;
+        } else {
+            int wynik = 1;
+            for (int i = 1; i <= n; i++) {
+                wynik *= i;
+            }
+            return wynik;
+        }
     }
 
-    public static void obliczStatystyki(double... liczby) {
-        if (liczby.length == 0) {
-            System.out.println("Brak danych do obliczeń.");
-            return;
-        }
-
-        double suma = 0;
-        for (double liczba : liczby) {
-            suma += liczba;
-        }
-
-        double srednia = suma / liczby.length;
-
-        // Sortowanie liczb w celu obliczenia mediany
-        java.util.Arrays.sort(liczby);
-
-        double mediana;
-        if (liczby.length % 2 == 0) {
-            int middle = liczby.length / 2;
-            //liczby[middle - 1] oznacza element po lewej stronie środka (gdy liczby są posortowane, to jest to element "niższy").
-            //liczby[middle] oznacza element po prawej stronie środka (element "wyższy").
-            mediana = (liczby[middle - 1] + liczby[middle]) / 2.0;
-        } else {
-            int middle = liczby.length / 2;
-            mediana = liczby[middle];
-        }
-
-        System.out.println("Suma: " + suma);
-        System.out.println("Średnia: " + srednia);
-        System.out.println("Mediana: " + mediana);
+    public static void main(String[] args) {
+        int liczba = 19; // Możesz zmienić tę liczbę na dowolną inną, dla której chcesz obliczyć silnię.
+        int wynik = obliczSilnie(liczba);
+        System.out.println("Silnia z " + liczba + " wynosi " + wynik);
     }
 }
