d7f22cb85bee712d995ca39333dbc540cac00488
VisioneXpress <118394744+VisioneXpress@users.noreply.github.com>
2023-10-18 18:52:18 +0200

    fibbonacci

--- Main.java
+++ Main.java
@@ -1,31 +1,25 @@
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.format.TextStyle;
-import java.util.Locale;
-
 public class Main {
+    public static int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        int fibNMinus2 = 0;
+        int fibNMinus1 = 1;
+        int fibN = 0;
+
+        for (int i = 2; i <= n; i++) {
+            fibN = fibNMinus1 + fibNMinus2;
+            fibNMinus2 = fibNMinus1;
+            fibNMinus1 = fibN;
+        }
+
+        return fibN;
+    }
 
     public static void main(String[] args) {
-        wypiszDzienTygodnia(); // Wywołanie funkcji bez parametru
-        wypiszDzienTygodnia(7 ); // Wywołanie funkcji z parametrem
-    }
-
-    // Funkcja bez parametru wypisująca aktualny dzień tygodnia
-    public static void wypiszDzienTygodnia() {
-        LocalDate currentDate = LocalDate.now();
-        DayOfWeek currentDayOfWeek = currentDate.getDayOfWeek();
-        String dayOfWeek = currentDayOfWeek.getDisplayName(TextStyle.FULL, Locale.getDefault());
-        System.out.println("Dzisiaj jest " + dayOfWeek);
-    }
-
-    // Funkcja z parametrem wypisująca dzień tygodnia na podstawie liczby
-    public static void wypiszDzienTygodnia(int dayNumber) {
-        if (dayNumber >= 1 && dayNumber <= 7) {
-            DayOfWeek selectedDayOfWeek = DayOfWeek.of(dayNumber);
-            String dayOfWeek = selectedDayOfWeek.getDisplayName(TextStyle.FULL, Locale.getDefault());
-            System.out.println("Dzień tygodnia o numerze " + dayNumber + " to " + dayOfWeek);
-        } else {
-            System.out.println("Nieprawidłowa liczba. Podaj liczbę od 1 do 7.");
-        }
+        int position = 12; // Zmień tę wartość na pozycję w ciągu, którą chcesz obliczyć
+        int result = fib(position);
+        System.out.println("Liczba na pozycji " + position + " w ciągu Fibonacciego to: " + result);
     }
 }
